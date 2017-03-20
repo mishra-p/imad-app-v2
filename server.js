ab8c6d3a453793d12a43d7483497e17e7fadf7f2
mishra-p <prashantmishrasunrise@gmail.com>
2017-03-20 13:26:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req,res){
    res.send('Article one is served here'); 
 });
-
+app.get('/article-two',function(req,res){
+   res.send('Article two is served here'); 
+});
+app.get('/article-three',function(req,res){
+   res.send('Article three is served here'); 
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
