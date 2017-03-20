6b55bbe3c2e76ea8b24516cd33ffc4d1b757dd86
mishra-p <prashantmishrasunrise@gmail.com>
2017-03-20 23:12:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,14 +98,11 @@ app.get('/article-one',function(req,res){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname,'ui', 'article-one.html')); 
-});
-app.get('/article-two',function(req,res){
-   res.sendfile(path.join(__dirname,'ui','article-two.html')); 
-});
-app.get('/article-three',function(req,res){
-   res.sendFile(path.join(__dirname,'ui','article-three.html')); 
+app.get('/:articleName',function(req,res){
+	//articleName==article-one 
+	//article[articleName]==content object for article one  
+   var articleName=req.params.articleName;   
+res.send(createTemplate(articles[articleName])); 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
