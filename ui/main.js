28af6094ee89b2d19b190e2c9b88d1f089f5a22c
mishra-p <prashantmishrasunrise@gmail.com>
2017-03-21 22:20:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var img=document.getElementById('hide');
+var marginLeft=0;
+function moveRight(){
+    marginLeft+=5;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function (){
+  var interval=setInterval(moveRight,50);  
+};
