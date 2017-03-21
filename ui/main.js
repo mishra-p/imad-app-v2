37a33fb9c37b881bcbb588bdd8ea1e58da99ea09
mishra-p <prashantmishrasunrise@gmail.com>
2017-03-22 02:10:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,5 @@
 //Counter code
-var button=document.getElementById("counter");
-var counter=0;
+var button=document.getElementById('counter');
 button.onclick=function(){
     //Create a request object to the counter endpoint
     var request=new XMLHttpRequest();
@@ -10,7 +9,7 @@ button.onclick=function(){
             //Take some action
             if(request.status===200){
                 var counter=request.responseText;
-                var span=document.getElementById("count");
+                var span=document.getElementById('count');
                 span.innerHTML=counter.toString();
             }
          }
@@ -19,3 +18,32 @@ button.onclick=function(){
         request.open('GET','http://mishra-p.imad.hasura-app.io/counter',true);
         request.send(null);
  };
+ 
+ //Submit name
+ var nameInput=document.getElementById('name');
+ var name=nameInput.value;
+ var submit=document.getElementById('submit_btn');
+ submit.onclick=function(){
+     //Make a request to the server and send the name
+     
+     //Capture a list of names and render it as a list
+     var names=['name1','name2','name3','name4'];
+     var list='';
+     for(var i=0;i<names.length;i++)
+        list+='<li>'+names[i]+'<li>';
+ };
+ var ul=document.getElementById('nameList');
+ ul.innerHTML=list;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
