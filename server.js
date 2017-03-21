fb369f63f31b3440f0ef5379c1cf111273429e0d
mishra-p <prashantmishrasunrise@gmail.com>
2017-03-22 02:18:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var counter=0;
+app.get('/counter', function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
+var names=[];   
+app.get('/submit-name/:name',function(req,res){
+   //Get the name from the request
+   var name=req.params.name;
+   
+   names.push(name);
+   //JSON:Javascript Object Notation...to convert object into string 
+   res.send(JSON.stringfy(names));
+});
+
 var articles={
    'article-one': {
 	title:'Article One | Prashant Mishra',
@@ -96,21 +112,6 @@ app.get('/:articleName',function(req,res){
 	//article[articleName]==content object for article one  
    var articleName=req.params.articleName;   
 res.send(createTemplate(articles[articleName])); 
-});
-var counter=0;
-app.get('/counter', function(req,res){
-    counter=counter+1;
-    res.send(counter.toString());
-});
-
-var names=[];   
-app.get('/submit-name/:name',function(req,res){
-   //Get the name from the request
-   var name=req.params.name;
-   
-   names.push(name);
-   //JSON:Javascript Object Notation...to convert object into string 
-   res.send(JSON.stringfy(names));
 });
 app.get('/ui/main.js',function(req,res){
     res.sendFile(path.join(__dirname,'ui','main.js'));
